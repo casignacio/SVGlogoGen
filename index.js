c5e240b8d66c8e0815ae92ddcbb75b328d911c62
casignacio <carloanthonyignacio@gmail.com>
2023-06-28 18:51:23 -0600

    updated index file

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ function writeToFile(fileName, answers) {
     svgString += "<g>";
     svgString += `${answers.shape}`;
 
-    let shapeChoice;
-    if (answers.shape === "Square") {
-        shapeChoice = new Square();
-        svgString += `<rect x="75" y="40" width="150" height="150" fill="${answers.shapeColor}"/>`;
+    let shape;
+    if (answers.shape === "Triangle") {
+        shape = new Triangle();
+        svgString += `<polygon points="150, 30 240, 180 60, 180" fill="${answers.shapeColor}"/>`;
     } else if (answers.shape === "Circle") {
-        shapeChoice = new Circle();
+        shape = new Circle();
         svgString += `<circle cx="150" cy="115" r="82" fill="${answers.shapeColor}"/>`;
     } else {
-        shapeChoice = new Triangle();
-        svgString += `<polygon points="150, 30 240, 180 60, 180" fill="${answers.shapeColor}"/>`;
+        shape = new Square();
+        svgString += `<rect x="75" y="40" width="150" height="150" fill="${answers.shapeColor}"/>`;
     }
 
     svgString += `<text x="150" y="130" font-size="45" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>`;
@@ -25,7 +25,7 @@ function writeToFile(fileName, answers) {
     svgString += "</svg>";
 
     fs.writeFile(fileName, svgString, (err) => {
-        err ? console.log(err) : console.log("Generated logo.svg");
+        err ? console.log(err) : console.log("logo.svg has been generated!");
     });
 }
 
@@ -33,25 +33,26 @@ function promptUser() {
     inquirer.prompt([
             {
                 type: "input",
-                message: "Enter desired text for the logo to display (up to 3 characters)",
+                message: "Enter text for logo (3 characters max)",
                 name:"text",
             },
             {
                 type: "input",
-                message: "Choose the desired color for the text (keyword OR hexadecimal number)",
+                message: "What color would you like your text to be? (enter keyword OR hexadecimal number)",
                 name:"textColor",
             },
             {
+                type: "input",
+                message: "Choose a background color! (enter keyword OR hexadecimal number)",
+                name:"shapeColor",
+            },
+            {
                 type: "list",
-                message: "Choose which shape your logo will be",
+                message: "Choose a shape!",
                 choices: ["Triangle", "Circle", "Square"],
                 name:"shape",
             },
-            {
-                type: "input",
-                message: "Choose the desired color for the logo shape (keyword OR hexadecimal number)",
-                name:"shapeColor",
-            },
+
         ])
         .then((answers) => {
             if (answers.text.length > 3) {
